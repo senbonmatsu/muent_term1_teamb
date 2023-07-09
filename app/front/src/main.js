fa48256c02b3f327f98112b22d381cb1517d1fed
senbonmatsu <s2222020@stu.musashino-u.ac.jp>
2023-07-10 01:02:30 +0900

    login

--- app/front/src/main.js
+++ app/front/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import axios from 'axios';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'http://127.0.0.1:8000';  // the FastAPI backend
 createApp(App).mount('#app')
